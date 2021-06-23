3ff674d6b7c9570c6f99c9de9557748065c79de6
Rodrigo <rodrigo_coelho@orange.fr>
2021-06-23 14:05:12 +0200

    equals button ID and modify operate function

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
+function operate(operator, sum, newNum) {
+    switch(operator) {
+        case "+":
+            sum = add(sum, newNum);
+            break;
+        case "-":
+            sum = subtract(sum, newNum);
+            break;
+        case "*":
+            sum = multiply(sum, newNum);
+            break;
+        case "/":
+            sum = divide(sum, newNum);
+            break;
+    }
+    return sum;
+}
+
+/* function operate(operator, num1, num2) {
     let sum = 0;
     switch(operator) {
         case "+":
@@ -31,14 +49,20 @@ function operate(operator, num1, num2) {
             break;
     }
     return sum;
-}
+} */
 
 const displayBar = document.getElementById("display-bar");
 displayBar.value = "0";
 
-const digitButtons = document.querySelectorAll(".digit-btn");
 const clearButton = document.getElementById("clear-btn");
+const digitButtons = document.querySelectorAll(".digit-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
+const equalsButton = document.getElementById("equals-btn");
+let sum = 0;
+
+clearButton.addEventListener("click", () => {
+    displayBar.value = "0";
+});
 
 digitButtons.forEach((digitButton) => {
     digitButton.addEventListener("click", () => {
@@ -47,8 +71,4 @@ digitButtons.forEach((digitButton) => {
         }
         displayBar.value += digitButton.textContent;
     });
-});
-
-clearButton.addEventListener("click", () => {
-    displayBar.value = "0";
 });
