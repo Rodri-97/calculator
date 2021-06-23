02089078521ca5b824426185e109b9e112107a3f
Rodrigo <rodrigo_coelho@orange.fr>
2021-06-23 15:27:23 +0200

    add operators functionality

--- script.js
+++ script.js
@@ -39,10 +39,14 @@ const clearButton = document.getElementById("clear-btn");
 const digitButtons = document.querySelectorAll(".digit-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
 const equalsButton = document.getElementById("equals-btn");
-let sum = 0;
+
+let numbers = [];
+let operators = [];
 
 clearButton.addEventListener("click", () => {
     displayBar.value = "0";
+    numbers = [];
+    operators = [];
 });
 
 digitButtons.forEach((digitButton) => {
@@ -52,4 +56,35 @@ digitButtons.forEach((digitButton) => {
         }
         displayBar.value += digitButton.textContent;
     });
+});
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+        const operator = operatorButton.textContent;
+        const newNumber = Number(displayBar.value);
+        operators.push(operator);
+        numbers.push(newNumber);
+        displayBar.value = "";
+    });
+});
+
+function getResult(numbers, operators) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result = operate(operators[i - 1], result, numbers[i]);
+    }
+    return result;
+}
+
+// ---- Test getResult() function ----
+//const numbers = [5, 6, 7, 8];
+//const operators = ["+", "-", "*"];
+//getResult(numbers, operators);
+// 5 + 6 = 11 --> 11 - 7 = 4 --> 4 * 8 = 32
+
+equalsButton.addEventListener("click", () => {
+    const lastNumber = Number(displayBar.value);
+    numbers.push(lastNumber);
+    const result = getResult(numbers, operators);
+    displayBar.value = String(result);
 });
